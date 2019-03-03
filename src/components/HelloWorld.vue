b83b65dad6872d3ac4414e365ca116a294c5e1ae
Jenny Xing <jennyxing27@gmail.com>
2019-03-03 14:02:46 -0500

    init vue files and components

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Coming Soon<sup>TM</sup>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-family: neutraTextBold, Futura, "Trebuchet MS", Arial, sans-serif;
+  font-size: 36px;
+}
+</style>
