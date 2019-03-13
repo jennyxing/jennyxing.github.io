217740f5fe35628313371caf9e681c53b8a6f290
Jenny Xing <jennyxing27@gmail.com>
2019-03-13 15:48:24 -0400

    add all the icons!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,16 @@
 <template>
-  <h1>Coming Soon<sup>TM</sup>
-  </h1>
+  <div>
+    <h1>Coming Soon<sup>TM</sup>
+    </h1>
+    <font-awesome-icon :icon="['fab', 'twitter']" />
+    <font-awesome-icon :icon="['fab', 'github']" />
+    <font-awesome-icon :icon="['fab', 'linkedin-in']" />
+    <font-awesome-icon icon="envelope" />
+    <font-awesome-icon :icon="['fab', 'goodreads-g']" />
+    <font-awesome-icon :icon="['fab', 'medium-m']" />
+    <font-awesome-icon :icon="['fab', 'dribbble']" />
+    <font-awesome-icon :icon="['fab', 'codepen']" />
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTwitter, faGithub, faGoodreadsG, faLinkedinIn, faMediumM, faDribbble, faCodepen } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTwitter, faGithub, faGoodreadsG, faLinkedinIn, faEnvelope, faMediumM, faDribbble, faCodepen)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
