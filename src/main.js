f49ee70e301836f98f69e284606f29a822f2bf33
Jenny Xing <contact@jennyxing.com>
2021-10-05 13:36:01 -0400

    remove goodreads icon import

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTwitter, faGithub, faGoodreadsG, faLinkedinIn, faMediumM, faDribbble, faCodepen } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faGithub, faLinkedinIn, faMediumM, faDribbble, faCodepen } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faTwitter, faGithub, faGoodreadsG, faLinkedinIn, faEnvelope, faMediumM, faDribbble, faCodepen)
+library.add(faTwitter, faGithub, faLinkedinIn, faEnvelope, faMediumM, faDribbble, faCodepen)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
